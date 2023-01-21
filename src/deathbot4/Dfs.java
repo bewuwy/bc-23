7ab1d957119690247f30ce7105b13e1b2c98d364
olo667 <uummaaj@gmail.com>
2023-01-21 14:16:19 +0100

    bumped to v4

---
+++ src/deathbot4/Dfs.java
@@ -0,0 +1,38 @@
+package deathbot4;
+
+import battlecode.common.*;
+
+public class Dfs {
+
+
+    Direction lastDfs;
+
+    void go(RobotController rc, Direction dir) throws GameActionException {
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            lastDfs = dir;
+        } else {
+            if (lastDfs != null) {
+                Direction[] dirs = {lastDfs, lastDfs.rotateLeft(), lastDfs.rotateRight(), lastDfs.rotateLeft().rotateLeft(), lastDfs.rotateRight().rotateRight()};
+                for (Direction d : dirs) {
+                    if (rc.canMove(d)) {
+                        rc.move(d);
+                        lastDfs = d;
+                        break;
+                    }
+                }
+            } else{
+                Direction[] dirs = {dir, dir.rotateLeft(), dir.rotateRight(), dir.rotateLeft().rotateLeft(), dir.rotateRight().rotateRight()};
+                for (Direction d : dirs) {
+                    if (rc.canMove(d)) {
+                        rc.move(d);
+                        lastDfs = d;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ src/deathbot4/Launcher.java
@@ -0,0 +1,111 @@
+package deathbot4;
+
+import deathbot3.Dfs;
+import java.util.ArrayList;
+import java.util.List;
+
+import battlecode.common.*;
+
+public class Launcher extends RobotPlayer {
+
+    public static final int LAUNCHERS_TO_ATTACK = 5;
+
+    public static void initLauncher(RobotController rc) throws GameActionException {
+
+        MapLocation myLoc = rc.getLocation();
+        robotDirection = ownHQ.directionTo(myLoc);
+    }
+
+    public static void runLauncher(RobotController rc) throws GameActionException {
+        // Try to attack someone
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] robotsNearby = rc.senseNearbyRobots(-1);
+        
+        List<RobotInfo> enemies = new ArrayList<RobotInfo>();
+
+        for (RobotInfo robot : robotsNearby) {
+            if (robot.getTeam() == opponent && robot.getType() != RobotType.HEADQUARTERS) {
+
+                enemies.add(robot);
+                break;
+            }
+        }
+
+        if (enemies.size() > 0) {
+            MapLocation toAttack = enemies.get(0).getLocation();
+
+            if (rc.canAttack(toAttack)) {
+                rc.setIndicatorString("Attacking");        
+                rc.attack(toAttack);
+            }
+        }
+
+        // Launcher movement
+        MapLocation myLocation = rc.getLocation();
+        if (returnToHQ) {
+
+            // If we are returning to HQ, move towards HQ
+            Direction dir = myLocation.directionTo(ownHQ);
+            dfs.go(rc, dir);      
+        } else {
+
+            //! move only towards the enemy HQ if there are many launchers nearby, otherwise go to the center
+            int numAllyLaunchersNearby = 0;
+            int enemiesHPSum = 0;
+            int alliesHPSum = 0;
+            MapLocation enemyHQ = new MapLocation(mapSize[0] - ownHQ.x, mapSize[1] - ownHQ.y); // fail-safe
+
+            for (RobotInfo robot : robotsNearby) {
+                if (robot.getType() == RobotType.LAUNCHER && robot.getTeam() == rc.getTeam()) {
+                    numAllyLaunchersNearby++;
+                    alliesHPSum += robot.getHealth();
+                }
+                if (robot.getType() == RobotType.HEADQUARTERS && robot.getTeam() == rc.getTeam().opponent()) {
+                    enemyHQ = robot.getLocation();
+                }
+                if (robot.getType() == RobotType.LAUNCHER && robot.getTeam() == rc.getTeam().opponent()) {
+                    enemiesHPSum += robot.getHealth();
+                }
+            }
+
+            // boolean move = true;
+            boolean opposite = false;
+            MapLocation launcherTargetLoc;
+
+            // attack
+            if (numAllyLaunchersNearby >= LAUNCHERS_TO_ATTACK) {   
+                launcherTargetLoc = enemyHQ;
+            }
+            else {
+                // go to center
+                launcherTargetLoc = new MapLocation(mapSize[0] / 2, mapSize[1] / 2);
+            }
+
+            // retreat if enemies stronger
+            if (enemiesHPSum > alliesHPSum) {
+                launcherTargetLoc = enemyHQ;
+                opposite = true;
+            }
+
+            // if (enemies.size() > 0) {
+                //? If there are enemies, move towards them -- stupid?
+            //     launcherTargetLoc = enemies.get(0).location;
+            // }
+
+            if (myLocation.distanceSquaredTo(enemyHQ) <= 9 && enemies.size() == 0) {
+                // If we are close to the enemy HQ, move away from it
+                launcherTargetLoc = enemyHQ;
+                opposite = true;
+            }
+
+            Direction dir = myLocation.directionTo(launcherTargetLoc);
+
+            if (opposite) {
+                dir = dir.opposite();
+            }
+
+            dfs.go(rc, dir);
+        }
+    }
+
+}
